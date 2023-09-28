f2c36739d868be46c9ddb53ad8a3307deb5925d4
McWared <mrbaymiks@gmail.com>
2023-09-28 18:24:21 +0300

    Added Counter, setState method

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       <Greet name="Andrew">
         <Welcome name="Andrew"/>
       </Greet>
